6298f714b969127c55a0a426a5a337f39581a995
Antonio <antonio@example.com>
2026-08-31 20:32:53 +0200

    Ignore args in case of the exception serialization.

--- src/main/java/com/mycompany/common/exception/MyCompanyException.java
+++ src/main/java/com/mycompany/common/exception/MyCompanyException.java
@@ -6,7 +6,7 @@ public class MyCompanyException extends RuntimeException {
 	private static final long serialVersionUID = -2169547160291888849L;
 
 	private final String msgKey;
-	private final Object[] args;
+	private final transient Object[] args;
 	private final HttpStatus status;
 
 	protected MyCompanyException(String msgKey, HttpStatus status, Object... args) {
